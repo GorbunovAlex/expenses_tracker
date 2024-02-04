21001837ea13f6f93718e82145bb0c58c3bcd728
Alexander Gorbunov <agorbunov@enty.io>
2024-02-04 10:47:56 +0100

    added quasar

---
+++ exptr_front/src/layouts/DefaultLayout.vue
@@ -0,0 +1,17 @@
+<script setup lang='ts'></script>
+
+<template>
+<q-layout view="lHh lpR lFf" class="bg-white">
+    <q-drawer
+      show-if-above
+      bordered
+      class="bg-grey-2"
+    >
+      
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
--- exptr_front/src/main.ts
+++ exptr_front/src/main.ts
@@ -1,7 +1,13 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+
+import { Quasar } from 'quasar'
+
+import '@quasar/extras/roboto-font-latin-ext/roboto-font-latin-ext.css'
+import '@quasar/extras/material-icons/material-icons.css'
+import '@quasar/extras/material-icons-outlined/material-icons-outlined.css'
+
+import 'quasar/src/css/index.sass'
 
 import App from './App.vue'
 import router from './router'
@@ -10,5 +16,8 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(Quasar, {
+  plugins: {}
+})
 
 app.mount('#app')
