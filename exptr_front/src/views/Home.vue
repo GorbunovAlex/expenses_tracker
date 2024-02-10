d306d5c78b8647b5859ba98c8b0502d1413319a8
Alexander Gorbunov <agorbunov@enty.io>
2024-02-10 06:08:07 +0100

    added charts

--- exptr_front/src/views/Home.vue
+++ exptr_front/src/views/Home.vue
@@ -1,9 +1,16 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import BarsChart from '@/components/analytics/BarsChart.vue'
+import DoughtChart from '@/components/analytics/DoughtnutChart.vue'
+</script>
 
 <template>
   <div class="home q-pa-md">
-    <div class=""></div>
-    <div class="">awd</div>
+    <div class="">
+      <BarsChart />
+    </div>
+    <div class="">
+      <DoughtChart />
+    </div>
     <div class="row items-center justify-end text-white text-bold text-h5">$: 1234</div>
   </div>
 </template>
